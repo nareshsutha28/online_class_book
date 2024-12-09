6554bb202347f8cbaf520ee46f3b168d3dd1071b
nareshsutha28 <nareshsutha28@gmail.com>
2024-12-09 19:41:47 +0530

    Setup postgresdb and add User abstractmodel

--- online_class_book/settings.py
+++ online_class_book/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'online_class_book.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
@@ -121,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Setup user model
+AUTH_USER_MODEL = 'user.User'
---
+++ user/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from user.managers import CustomUserManager
+
+
+# Create your models here.
+class User(AbstractUser):
+
+    class UserRole(models.TextChoices):
+        STUDENT = 'Student', 'Student'
+        TEACHER = 'Teacher', 'Teacher'
+
+    # Field declarations
+    username = None
+    email = models.EmailField(unique = True)
+    age = models.PositiveIntegerField(null=True)
+    phone = models.CharField(max_length=10)
+    role = models.CharField(max_length=20, choices=UserRole.choices)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'role']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+      return "{}".format(self.email)
